dc75796f328d9c89a30ad14866d4df8a2f9e1fdc
kamyaa07 <53122015+kamyaa07@users.noreply.github.com>
2021-05-22 22:25:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,236 @@
+/*--------------------------------------------------------*/
+var PLAY = 1;
+var END = 0;
+var WIN = 2;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var jungle, invisiblejungle;
+
+var obstaclesGroup, obstacle1;
+
+var score=0;
+
+var gameOver, restart;
+
+function preload(){
+  kangaroo_running =   loadAnimation("assets/kangaroo1.png","assets/kangaroo2.png","assets/kangaroo3.png");
+  kangaroo_collided = loadAnimation("assets/kangaroo1.png");
+  jungleImage = loadImage("assets/bg.png");
+  shrub1 = loadImage("assets/shrub1.png");
+  shrub2 = loadImage("assets/shrub2.png");
+  shrub3 = loadImage("assets/shrub3.png");
+  obstacle1 = loadImage("assets/stone.png");
+  gameOverImg = loadImage("assets/gameOver.png");
+  restartImg = loadImage("assets/restart.png");
+  jumpSound = loadSound("assets/jump.wav");
+  collidedSound = loadSound("assets/collided.wav");
+  
+  trex_running = loadAnimation("images/trex1.png","images/trex3.png","images/trex4.png");
+  trex_collided = loadAnimation("images/trex_collided.png");
+  
+  groundImage = loadImage("images/ground2.png");
+  
+  cloudImage = loadImage("images/cloud.png");
+  
+  obstacle1 = loadImage("images/obstacle1.png");
+  obstacle2 = loadImage("images/obstacle2.png");
+  obstacle3 = loadImage("images/obstacle3.png");
+  obstacle4 = loadImage("images/obstacle4.png");
+  obstacle5 = loadImage("images/obstacle5.png");
+  obstacle6 = loadImage("images/obstacle6.png");
+  
+  restartImg = loadImage("images/restart.png")
+  gameOverImg = loadImage("images/gameOver.png")
+}
+
+function setup() {
+  createCanvas(800, 400);
+
+  jungle = createSprite(400,100,400,20);
+  jungle.addImage("jungle",jungleImage);
+  jungle.scale=0.3
+  jungle.x = width /2;
+  trex = createSprite(50,180,20,50);
+  
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided", trex_collided);
+  
+
+  trex.scale = 0.5;
+  
+  
+    
+  invisibleGround = createSprite(400,350,1600,10);
+  invisibleGround.visible = false;
+
+  gameOver = createSprite(400,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(550,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.1;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  
+  shrubsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+
+}
+
+function draw() {
+  background(255);
+  
+  trex.x=camera.position.x-270;
+   
+  if (gameState===PLAY){
+
+    jungle.velocityX=-3
+
+    if(jungle.x<100)
+    {
+       jungle.x=400
+    }
+   console.log(trex.y)
+    if(keyDown("space")&& trex.y>270) {
+      jumpSound.play();
+      trex.velocityY = -16;
+    }
+  
+    trex.velocityY = trex.velocityY + 0.8
+    spawnShrubs();
+    spawnObstacles();
+
+    trex.collide(invisibleGround);
+    
+    if(obstaclesGroup.isTouching(trex)){
+      collidedSound.play();
+      gameState = END;
+    }
+    if(shrubsGroup.isTouching(trex)){
+      score = score + 1;
+      shrubsGroup.destroyEach();
+    }
+  }
+  else if (gameState === END) {
+    gameOver.x=camera.position.x;
+    restart.x=camera.position.x;
+    gameOver.visible = true;
+    restart.visible = true;
+    //set velcity of each game object to 0
+    trex.velocityY = 0;
+    jungle.velocityX = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    shrubsGroup.setVelocityXEach(0);
+
+    //change the trex animation
+    trex.changeAnimation("collided",trex_collided);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    shrubsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+        reset();
+    }
+  }
+
+  else if (gameState === WIN) {
+    //set velcity of each game object to 0
+    jungle.velocityX = 0;
+    trex.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    shrubsGroup.setVelocityXEach(0);
+    
+    //change the trex animation
+    trex.changeAnimation("collided",trex_collided);
+
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    shrubsGroup.setLifetimeEach(-1);
+  }
+  
+  
+  drawSprites();
+
+  textSize(20);
+  stroke(3);
+  fill("black")
+  text("Score: "+ score, camera.position.x,50);
+  
+  if(score >= 5){
+    trex.visible = false;
+    textSize(30);
+    stroke(3);
+    fill("black");
+    text("Congragulations!! You win the game!! ", 70,200);
+    gameState = WIN;
+  }
+}
+
+function spawnShrubs() {
+  //write code here to spawn the clouds
+  if (frameCount % 150 === 0) {
+
+    var shrub = createSprite(camera.position.x+500,330,40,10);
+
+    shrub.velocityX = -(6 + 3*score/100)
+    shrub.scale = 0.6;
+
+    var rand = Math.round(random(1,3));
+    switch(rand) {
+      case 1: shrub.addImage(shrub1);
+              break;
+      case 2: shrub.addImage(shrub2);
+              break;
+      case 3: shrub.addImage(shrub3);
+              break;
+      default: break;
+    }
+    
+    //assign scale and lifetime to the shrub           
+    shrub.scale = 0.05;
+     //assign lifetime to the variable
+    shrub.lifetime = 400;
+    
+    shrub.setCollider("rectangle",0,0,shrub.width/2,shrub.height/2)
+    //add each cloud to the group
+    shrubsGroup.add(shrub);
+    
+  }
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 120 === 0) {
+
+    var obstacle = createSprite(camera.position.x+400,330,40,40);
+    obstacle.setCollider("rectangle",0,0,200,200)
+    obstacle.addImage(obstacle1);
+    obstacle.velocityX = -(6 + 3*score/100)
+    obstacle.scale = 0.15;
+    //assign scale and lifetime to the obstacle           
+
+    obstacle.lifetime = 400;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+    
+  }
+}
+
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  trex.visible = true;
+  trex.changeAnimation("running", trex_running);
+  obstaclesGroup.destroyEach();
+  shrubsGroup.destroyEach();
+  score = 0;
+}
